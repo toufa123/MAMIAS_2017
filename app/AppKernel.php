76bf3c9e75df8c6128f60b6759530e1971d9fed5
Atef OUERGHI <atef.ouerghi@gmail.com>
2016-10-25 17:10:28 +0100

    25102016 17h00

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,13 +8,98 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
+            // SYMFONY STANDARD EDITION
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            
+            // DOCTRINE
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+
+            // KNP HELPER BUNDLES
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+            //new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+
+            // USER
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+
+            // PAGE
+            //new Sonata\PageBundle\SonataPageBundle(),
+            //new Application\Sonata\PageBundle\ApplicationSonataPageBundle(),
+
+            // NEWS
+            new Sonata\NewsBundle\SonataNewsBundle(),
+            new Application\Sonata\NewsBundle\ApplicationSonataNewsBundle(),
+
+            // MEDIA
+            new Sonata\MediaBundle\SonataMediaBundle(),
+            new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+            // new Liip\ImagineBundle\LiipImagineBundle(),
+
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+
+            // Disable this if you don't want the audit on entities
+            //new SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle(),
+
+            // API
+            // new FOS\RestBundle\FOSRestBundle(),
+            //new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+
+            new JMS\SerializerBundle\JMSSerializerBundle($this),
+            //new FOS\CommentBundle\FOSCommentBundle(),
+            //new Sonata\CommentBundle\SonataCommentBundle(),
+            //new Application\Sonata\CommentBundle\ApplicationSonataCommentBundle(),
+
+            // SONATA CORE & HELPER BUNDLES
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\IntlBundle\SonataIntlBundle(),
+            new Sonata\FormatterBundle\SonataFormatterBundle(),
+            new Sonata\CacheBundle\SonataCacheBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\SeoBundle\SonataSeoBundle(),
+            new Sonata\ClassificationBundle\SonataClassificationBundle(),
+            new Application\Sonata\ClassificationBundle\ApplicationSonataClassificationBundle(),
+            new Sonata\NotificationBundle\SonataNotificationBundle(),
+            new Application\Sonata\NotificationBundle\ApplicationSonataNotificationBundle(),
+            //new Application\Sonata\SeoBundle\ApplicationSonataSeoBundle(),
+            new Sonata\DatagridBundle\SonataDatagridBundle(),
+            //new Sonata\TranslationBundle\SonataTranslationBundle(),
+
+            // Search Integration
+            //new FOS\ElasticaBundle\FOSElasticaBundle(),
+
+            // CMF Integration
+            //new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
+
+            // Disable this if you don't want the timeline in the admin
+            new Spy\TimelineBundle\SpyTimelineBundle(),
+            
+            new Sonata\TimelineBundle\SonataTimelineBundle(),
+            new Application\Sonata\TimelineBundle\ApplicationSonataTimelineBundle(), // easy extends integration
+
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            
+            new Ivory\GoogleMapBundle\IvoryGoogleMapBundle(),
+            new Shtumi\UsefulBundle\ShtumiUsefulBundle(),
+            new PUGX\AutocompleterBundle\PUGXAutocompleterBundle(),
+            new Genemu\Bundle\FormBundle\GenemuFormBundle(),
+            new Ob\HighchartsBundle\ObHighchartsBundle(),
+            
+            //MAMIAS Application
             new MAMIAS\NISBundle\MAMIASNISBundle(),
         );
 

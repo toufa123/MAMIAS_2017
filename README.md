57eb22db2c05263cc71cf6019f6acc56b87e8955
Atef OUERGHI <atef.ouerghi@gmail.com>
2016-10-25 15:11:30 +0100

    25102016

---
+++ README.md
@@ -0,0 +1,4 @@
+MAMIAS_2017
+===========
+
+A Symfony project created on October 25, 2016, 1:15 pm.
---
+++ src/MAMIAS/NISBundle/MAMIASNISBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MAMIAS\NISBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MAMIASNISBundle extends Bundle
+{
+}
